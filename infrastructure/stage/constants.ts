38a04e8b837d7cab39f6f5cd98ce1233fdbd8641
Alexis Lucattini <Alexis.Lucattini@umccr.org>
2025-10-20 16:11:08 +1100

    Add is new workflow manager deployed boolean to staging account

--- infrastructure/stage/constants.ts
+++ infrastructure/stage/constants.ts
@@ -106,7 +106,7 @@ export const SSM_SCHEMA_ROOT = path.join(SSM_PARAMETER_PATH_PREFIX, 'schemas');
 /* Future proofing */
 export const NEW_WORKFLOW_MANAGER_IS_DEPLOYED: Record<StageName, boolean> = {
   BETA: true,
-  GAMMA: false,
+  GAMMA: true,
   PROD: false,
 };
 
