79773c4afe3f6c97359efb33727a51755cbca0b1
Alexis Lucattini <Alexis.Lucattini@umccr.org>
2026-05-21 16:15:08 +1000

    feat(payload-versioning): Define payload version types and constants

--- infrastructure/stage/event-schemas/interfaces.ts
+++ infrastructure/stage/event-schemas/interfaces.ts
@@ -1,7 +1,10 @@
+import { PayloadVersionType } from '../interfaces';
+
 export type SchemaNames = 'completeDataDraft';
 
 export const schemaNamesList: SchemaNames[] = ['completeDataDraft'];
 
 export interface BuildSchemaProps {
   schemaName: SchemaNames;
+  payloadVersion: PayloadVersionType;
 }
