efb092d1d93c7189026913f9e665cc5f091d7ca3
Alexis Lucattini <Alexis.Lucattini@umccr.org>
2026-05-21 16:15:09 +1000

    feat(event-schemas): Implement versioned schema deployment in CDK

--- infrastructure/stage/event-schemas/index.ts
+++ infrastructure/stage/event-schemas/index.ts
@@ -1,6 +1,7 @@
 import * as schemas from 'aws-cdk-lib/aws-eventschemas';
 import * as ssm from 'aws-cdk-lib/aws-ssm';
 import {
+  DEFAULT_PAYLOAD_VERSION,
   EVENT_SCHEMAS_DIR,
   SCHEMA_REGISTRY_NAME,
   SSM_SCHEMA_ROOT,
@@ -11,44 +12,65 @@ import * as fs from 'fs';
 import { schemaNamesList, BuildSchemaProps } from './interfaces';
 import { Construct } from 'constructs';
 import { camelCaseToKebabCase } from '../utils';
+import { payloadVersionList } from '../interfaces';
 
 export function buildSchema(scope: Construct, props: BuildSchemaProps): schemas.CfnSchema {
   // Import the schema file from the schemas directory
   const schemaPath = path.join(
     EVENT_SCHEMAS_DIR,
-    camelCaseToKebabCase(props.schemaName) + '-schema.json'
+    camelCaseToKebabCase(props.schemaName),
+    props.payloadVersion,
+    'schema.json'
   );
 
   // Create a new schema in the Event Schemas service
-  return new schemas.CfnSchema(scope, props.schemaName, {
+  return new schemas.CfnSchema(scope, `${props.schemaName}--${props.payloadVersion}--schema`, {
     type: 'JSONSchemaDraft4',
     content: fs.readFileSync(schemaPath, 'utf-8'),
     registryName: SCHEMA_REGISTRY_NAME,
-    schemaName: `${STACK_PREFIX}--${props.schemaName}`,
+    schemaName: `${STACK_PREFIX}--${props.schemaName}--${props.payloadVersion}`,
   });
 }
 
 export function buildSchemas(scope: Construct) {
   // Add an ssm entry for the registry name
-  new ssm.StringParameter(scope, `${SCHEMA_REGISTRY_NAME}-ssm`, {
+  new ssm.StringParameter(scope, `${SCHEMA_REGISTRY_NAME}--ssm`, {
     parameterName: path.join(SSM_SCHEMA_ROOT, 'registry'),
     stringValue: SCHEMA_REGISTRY_NAME,
   });
 
   // Iterate over the schemas directory and create a schema for each file
   for (const schemaName of schemaNamesList) {
-    const schemaObj = buildSchema(scope, {
-      schemaName: schemaName,
-    });
-    // And also a latest ssm parameter for the schema
-    // Likely the one most commonly used
-    new ssm.StringParameter(scope, `${schemaName}-ssm-latest`, {
-      parameterName: path.join(SSM_SCHEMA_ROOT, camelCaseToKebabCase(schemaName), 'latest'),
-      stringValue: JSON.stringify({
-        registryName: schemaObj.registryName,
-        schemaName: schemaObj.attrSchemaName,
-        schemaVersion: schemaObj.attrSchemaVersion,
-      }),
-    });
+    if (schemaName === 'completeDataDraft') {
+      for (const payloadVersion of payloadVersionList) {
+        const schemaObj = buildSchema(scope, {
+          schemaName: schemaName,
+          payloadVersion: payloadVersion,
+        });
+        new ssm.StringParameter(scope, `${schemaName}-${payloadVersion}--ssm`, {
+          parameterName: path.join(
+            SSM_SCHEMA_ROOT,
+            camelCaseToKebabCase(schemaName),
+            payloadVersion
+          ),
+          stringValue: JSON.stringify({
+            registryName: schemaObj.registryName,
+            schemaName: schemaObj.attrSchemaName,
+            schemaVersion: schemaObj.attrSchemaVersion,
+          }),
+        });
+        // And also an ssm parameter for the default used schema
+        if (payloadVersion === DEFAULT_PAYLOAD_VERSION) {
+          new ssm.StringParameter(scope, `${schemaName}-default--ssm`, {
+            parameterName: path.join(SSM_SCHEMA_ROOT, camelCaseToKebabCase(schemaName), 'default'),
+            stringValue: JSON.stringify({
+              registryName: schemaObj.registryName,
+              schemaName: schemaObj.attrSchemaName,
+              schemaVersion: schemaObj.attrSchemaVersion,
+            }),
+          });
+        }
+      }
+    }
   }
 }
