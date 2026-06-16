d9d78b8661f335585544dfbf5bbcd62df44b8069
Alexis Lucattini <alexis.lucattini@umccr.org>
2026-06-16 20:22:54 +1000

    Add in git-commit-id to cloudformation stack (#60)

--- infrastructure/stage/stateful-application-stack.ts
+++ infrastructure/stage/stateful-application-stack.ts
@@ -3,10 +3,11 @@ import { Construct } from 'constructs';
 import { StatefulApplicationStackConfig } from './interfaces';
 import { buildSsmParameters } from './ssm';
 import { buildSchemas } from './event-schemas';
+import { GitStack } from '@orcabus/platform-cdk-constructs/deployment-stack-pipeline';
 
 export type StatefulApplicationStackProps = cdk.StackProps & StatefulApplicationStackConfig;
 
-export class StatefulApplicationStack extends cdk.Stack {
+export class StatefulApplicationStack extends GitStack {
   constructor(scope: Construct, id: string, props: StatefulApplicationStackProps) {
     super(scope, id, props);
 
